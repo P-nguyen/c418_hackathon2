c16eeca0168b6b4e2dc2cee96b1b3d40076f193f
jhonalino <jhonfrancisalino@gmail.com>
2018-06-05 15:18:55 -0700

    modified countries api for result page

--- resultsPage.js
+++ resultsPage.js
@@ -1,5 +1,12 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-    //grab url params here
+  //grab url params here
+  const { search } = window.location;
+  const [, countryCode] = search.split("=");
+  const countryName = CountryApi.getCountryNameFromCode(countryCode);
+  const countryLogoUrl = CountryApi.getCountryLogoUrl(countryCode)
+  console.log(countryLogoUrl);
+  $('h2.name').text(countryName);
+  $('.flag img').attr('src',countryLogoUrl);
 }
