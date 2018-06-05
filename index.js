88d1f3ba391c2c1e1f106fdf4a4382fced657768
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-06-05 15:26:22 -0700

    autoComplete with return key function to complete countryName

--- index.js
+++ index.js
@@ -9,24 +9,45 @@ function capitalizeStr( inputStr ){
 }
 
 function autoComplete( input$Ele, countryArray){
+    let lastFoundCountries;
 
-    input$Ele.on('input', function(event){
-
+    input$Ele.on('input', function(){
         let val = this.value; //this is the input dom element
         //close any open list.
         if (!val) {
             $(".autoCompleteBackground").text( "" );
-            return false;} // this is when someone deletes previous inputs its a ''
+            lastFoundCountries = null;
+            return;} // this is when someone deletes previous inputs its a ''
 
-        const filteredCountryArr = countryArray
-                                    .filter( country => country.substr(0, val.length).toUpperCase() === val.toUpperCase());
+        let filteredCountryArr = countryArray
+                            .filter( country => country.substr(0, val.length).toUpperCase() === val.toUpperCase() ? true : false );
+
+        //this is a check to see if array has anything, if nothing exist then stop the function.
+        if (filteredCountryArr.length === 0){
+            return $(".autoCompleteBackground").text( "" );}
 
         this.value = capitalizeStr(val);
 
-        const autoCompString = filteredCountryArr
-                                .splice(0,5)
-                                .reduce( (fullString, country) => `${fullString} | ${country}`);
-
+        lastFoundCountries = filteredCountryArr.splice(0,5);
+        const autoCompString = autoCompleteString( lastFoundCountries );
         $(".autoCompleteBackground").text( autoCompString );
+
     });
+
+    input$Ele.on("keypress", function(event){
+        if(event.keyCode === 13){
+            if(lastFoundCountries<1){
+                return $(".autoCompleteBackground").text( "" );
+            }
+
+            this.value = lastFoundCountries[0];
+            $(".autoCompleteBackground").text( autoCompleteString( lastFoundCountries ));
+            lastFoundCountries.splice(0,1);
+            return;
+        }
+    });
+
+    function autoCompleteString( inputCountryArray ) {
+        return inputCountryArray.reduce( (fullString, country) => `${fullString} | ${country}`);
+    }
 }
