c76a6d3ec96c9e495bd587a88822374b8b3384a6
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-06-05 12:17:42 -0700

    basic AutoComplete Function

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-    
+    autoComplete( $("#inputNav"), _countries);
+}
+
+function capitalizeStr( inputStr ){
+    return inputStr[0].toUpperCase() + inputStr.substring(1, inputStr.length);
+}
+
+function autoComplete( input$Ele, countryArray){
+
+    input$Ele.on('input', function(event){
+
+        let val = this.value; //this is the input dom element
+        //close any open list.
+        if (!val) {
+            $(".ghostInputNav").text( "" );
+            return false;} // this is when someone deletes previous inputs its a ''
+
+        const filteredCountryArr = countryArray
+                                    .filter( country => country.substr(0, val.length).toUpperCase() === val.toUpperCase());
+
+        this.value = capitalizeStr(val);
+
+        const autoCompString = filteredCountryArr
+                                .splice(0,5)
+                                .reduce( (fullString, country) => `${fullString} | ${country}`);
+
+        $(".ghostInputNav").text( autoCompString );
+    });
 }
