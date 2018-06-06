6c3262544c5d3c3c1a7cdca62b3114fdfe54d0d6
jhonalino <jhonfrancisalino@gmail.com>
2018-06-05 20:30:14 -0700

    added modified youtubeapi result resultspage youtube

--- resultsPage.js
+++ resultsPage.js
@@ -4,9 +4,25 @@ function initializeApp() {
   //grab url params here
   const { search } = window.location;
   const [, countryCode] = search.split("=");
+
   const countryName = CountryApi.getCountryNameFromCode(countryCode);
-  const countryLogoUrl = CountryApi.getCountryLogoUrl(countryCode)
-  console.log(countryLogoUrl);
-  $('h2.name').text(countryName);
-  $('.flag img').attr('src',countryLogoUrl);
+  const countryLogoUrl = CountryApi.getCountryLogoUrl(countryCode);
+
+  renderCountryName(countryName);
+  renderLogoImage(countryLogoUrl);
+//get the food of the country and pass it to servercall youtube
+  YoutubeApi.youtubeServerCall("goat water");
+  addEventHandlers();
+}
+
+function addEventHandlers() {
+  $(".modal").on("click", closeYoutubeModal);
+}
+
+function renderCountryName(name) {
+  $("h2.name").text(name);
+}
+
+function renderLogoImage(url) {
+  $(".flag img").attr("src", url);
 }
