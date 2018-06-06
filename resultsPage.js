80c39f56b744f3d7dd0d2b8ee834bae56270eb33
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-06-05 20:22:25 -0700

    ajax call to google images

--- resultsPage.js
+++ resultsPage.js
@@ -9,4 +9,30 @@ function initializeApp() {
   console.log(countryLogoUrl);
   $('h2.name').text(countryName);
   $('.flag img').attr('src',countryLogoUrl);
+  $('.brand').on('click', returnToHomepage );
+  ajaxGoogleImageSearch();
+}
+
+function returnToHomepage(){
+    window.location.href = ( "index.html" );
+}
+
+function ajaxGoogleImageSearch( inputFoodStr ){
+    var ajaxObject = {
+        dataType: 'json',
+        data:{
+          key: "AIzaSyCV18q5ZdhaazVuu7Msq2td6RMUbSKb_o8",
+            q: inputFoodStr,
+            type: 'image/jpeg',
+            imgSize: 'huge',
+            cx: "010569814504410284789:dq0xetlzofa"
+        },
+        url: "https://www.googleapis.com/customsearch/v1",
+        method: "GET",
+        success: function(response){
+          console.log(response);
+          console.log(response.items["1"].pagemap.cse_image["0"].src);
+        }
+    }
+    $.ajax(ajaxObject);
 }
